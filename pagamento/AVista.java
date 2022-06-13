575333a44e45a8965ca58ec3912b96d1df0d7dc4
fabio.nogueira.carmo <fabio.nogueira@soitic.com>
2022-06-13 19:13:44 -0300

    Alterando o valor do desconto a vista para double

--- pagamento/AVista.java
+++ pagamento/AVista.java
@@ -1,9 +1,9 @@
 package pck.pagamento;
 
 public class AVista extends Pagamento{
-	private int desconto;
+	private double desconto;
 	
-	public AVista(double valor,int desconto) {
+	public AVista(double valor,double desconto) {
 		super(valor);
 		if(desconto > 0 && desconto <= 100) {
 			this.setDesconto(desconto);
@@ -14,16 +14,18 @@ public class AVista extends Pagamento{
 		
 	}
 
-	public int getDesconto() {
-		return desconto;
+	public double getDesconto() {
+		return this.desconto;
 	}
 
-	public void setDesconto(int desconto) {
+	public void setDesconto(double desconto) {
 		this.desconto = desconto;
 	}
 	
 	public void exibirValorAVista() {
-		double valorAVista = this.getValor() + (this.getValor()*this.getDesconto());
+		double valorAVista = 0;
+		double valorDoDesconto = this.getDesconto()/100;		
+		valorAVista = this.getValor() - (this.getValor() * valorDoDesconto);
 		System.out.println("O valor final a vista com desconto é: " + valorAVista);
 	}
 }
